b508104899f923dbbaca3a99582b5b42c7b016a6
Fenitra Randriamihajanirina <fenitra.randriamihajanirina@pulse.mg>
2024-10-21 14:53:01 +0300

    sdf

--- src/app/components/main-content/layout/pages/home/home.component.ts
+++ src/app/components/main-content/layout/pages/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
 import { HistoriqueRegionComponent } from "./historique-region/historique-region.component";
 import { NatureBioComponent } from "./nature-bio/nature-bio.component";
+import { TourismeComponent } from "./tourisme/tourisme.component";
+import { CuisineComponent } from "./cuisine/cuisine.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, HistoriqueRegionComponent, NatureBioComponent],
+  imports: [HeroComponent, HistoriqueRegionComponent, NatureBioComponent, TourismeComponent, CuisineComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
